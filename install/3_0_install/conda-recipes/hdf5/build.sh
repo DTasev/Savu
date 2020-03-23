5f18e92ddd86844786f851e19ea49dc95d8ce4d2
Dimitar Tasev <dimitar.tasev@stfc.ac.uk>
2020-03-23 12:44:51 +0000

    run make with jobs equal to number of cpus

--- install/3_0_install/conda-recipes/hdf5/build.sh
+++ install/3_0_install/conda-recipes/hdf5/build.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# set compiler wrapper
-mpicc=$(command -v mpicc)
-mpi=${mpicc%/bin/mpicc}
-# export LD_LIBRARY_PATH=$mpi:$mpi/lib:$mpi/include:$LD_LIBRARY_PATH
 export LD_RUN_PATH=$LD_LIBRARY_PATH
 
-# check anaconda distribution
-# ana_path=$(command -v savu)
-
+# set compiler wrapper
+mpicc=$(command -v mpicc)
 CC=$mpicc ./configure --with-zlib --enable-parallel --enable-shared --prefix=$PREFIX
-make
+
+export NPROCS=$(nproc)
+
+make -j$NPROCS
 # Sets the real amount of cores available.
 # HDF5 tries to allocate 6 by default,
 # which will cause an error if the build machine
 # doesn't have that many
-export NPROCS=$(nproc)
 # make check
 make install
 
